baa812a088780417d81b69902e9f4e9c64daacfb
Add阿咚东 <759893882@qq.com>
2022-01-11 21:16:46 +0800

    根据白天tokenizer的实验，加入两种文本预处理的策略 对英文字母和数字连续出现的情况加入空格字符进行分割，以保证tokenizer准确 去除文本中关于日期的表述，明显带有年月日中两个元素连续出现的表述

---
+++ script/pre_process_for_text.py
@@ -0,0 +1,38 @@
+# 需要对文本进行预处理
+import re
+
+
+def better_tokenizer_pre_process(text: str):
+    ans = ''
+    for i in range(len(text) - 1):
+        if (text[i + 1].isdigit() and text[i].encode('utf-8').isalpha()) or (
+                text[i].isdigit() and text[i + 1].encode('utf-8').isalpha()):
+            ans += text[i]
+            ans += ' '
+        else:
+            ans += text[i]
+    return ans
+
+
+def removeTime(text: str):
+    # 去除三个包含日期的表述，2022年1月11日；2022年1月；1月11日
+    # 保留余下的，例如单独的患高血压4年，腹泻4日，心律不齐11月这样的描述
+    mode = [
+        '\d+年\d+月\d+日',
+        '\d+年\d+月',
+        '\d+月\d+日',
+    ]
+    pattern = "|".join(mode)
+    return re.sub(pattern, '##', text)
+
+
+def pre_process(text: str):
+    text = removeTime(text)
+    text = better_tokenizer_pre_process(text)
+    return text
+
+
+if __name__ == '__main__':
+    print(better_tokenizer_pre_process('患者因便血约3月门急诊拟 直肠癌入院。入院查体:T36.4℃,P82次/分,R20次/分,BP152/55mmHg'))
+    print(removeTime('1月11日患者因便血约3月门急诊拟 直肠癌入院。入院查体:T36.4℃,P82次/分,R20次/分,BP152/55mmHg，例如单独的患高血压4年，腹泻4日，心律不齐11月这样的描述'))
+    print(pre_process('1月11日患者因便血约3月门急诊拟 直肠癌入院。入院查体:T:36.4℃,P82次/分,R20次/分,BP152/55mmHg，例如单独的患高血压4年，腹泻4日，心律不齐11月这样的描述'))
