caa23e4909959fdfa26ac51ac929a90eadaa10c6
Add阿咚东 <759893882@qq.com>
2022-01-11 21:14:55 +0800

    prompt自动化生成的init

---
+++ get_prompt/test_fill_mask.py
@@ -0,0 +1,9 @@
+import json
+
+from transformers import pipeline
+
+unmasker = pipeline('fill-mask', model='../../bert-base-chinese')
+
+t = unmasker("北京是[MASK]国的首[MASK]")
+
+print(json.dumps(t, indent=4, ensure_ascii=False))
