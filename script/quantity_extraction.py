722d26ec4bdb509df6cd16e97bf7572acc841fbb
Add阿咚东 <759893882@qq.com>
2022-01-19 15:29:00 +0800

    支持数值0，和在抽取自身test函数里面加入对数值个数情况的统计

--- script/quantity_extraction.py
+++ script/quantity_extraction.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
 import re
+from collections import Counter
 
 import pandas as pd
 
@@ -8,7 +9,7 @@ dataframe = pd.read_csv('../data/unit.csv', header=None)
 unit_list = list(dataframe[0])
 
 base_mode = [
-    '0\.\d*',  # 0.35 mode
+    '0\.?\d*',  # 0.35 mode
     '[1-9]\d*\.?\d*',  # normal digital mode
 ]
 
@@ -24,8 +25,10 @@ quantity_mode = [
                     base_mode[1] + '×?' + base_mode[1],  # 3*4cm mode
                     base_mode[0] + '-?' + base_mode[0],
                     base_mode[1] + '-?' + base_mode[1],  # 6-7次/分 mode
+                    base_mode[0] + '-?' + base_mode[1],  # 0-7次/分 mode,
                     base_mode[0] + '/?' + base_mode[0],
-                    base_mode[1] + '/?' + base_mode[1],  # 159/87mmHg mode
+                    base_mode[1] + '/?' + base_mode[1],  # 159/87mmHg mode,
+                    base_mode[0] + '/?' + base_mode[1],  # 0/87mmHg mode
                 ] + base_mode
 
 
@@ -47,5 +50,7 @@ def extract_quantity(text: str):
 if __name__ == '__main__':
     all = extract_quantity(input())
     print(len(all))
-    for quantity in all:
-        print(quantity.value)
+    quantities=[quantity.value for quantity in all]
+    for quantity in quantities:
+        print(quantity)
+    print(Counter(quantities))
